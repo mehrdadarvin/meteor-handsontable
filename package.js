7317549bd9a3020f24177e53b6269ef2d72dc796
Mehrdad Arvin <mehrdad.arvin@gmail.com>
2015-04-20 13:21:52 -0600

    Added the github link

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: 'Handsontable is a minimalistic approach to Excel-like table editor in HTML & jQuery',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/mehrdadarvin/meteor-handsontable.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
