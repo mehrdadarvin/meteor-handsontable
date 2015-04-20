da0543b7851576986f9161ee8116e53b8fbf5228
Mehrdad Arvin <mehrdad.arvin@gmail.com>
2015-04-20 13:57:13 -0600

    tests added

---
+++ handsontable-tests.js
@@ -0,0 +1,9 @@
+if (Meteor.isClient) {
+  Tinytest.add('$.fn.handsontable & Handsontable exists', function (test) {
+    test.notEqual(jQuery.fn.handsontable, undefined);
+  });
+
+  Tinytest.add('Handsontable exists', function (test) {
+    test.notEqual(window.Handsontable, undefined);
+  });
+}
